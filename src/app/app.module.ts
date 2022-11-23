2e51b95a4526fa46a443eb249e26e27fce52a48d
AyeshaSatti <ayeshasaghir@gmail.com>
2022-11-23 14:46:55 +0500

    Assignment-Component-Databinding Input & Output use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { GamecontrolComponent } from './gamecontrol/gamecontrol.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    GamecontrolComponent,
+    OddComponent,
+    EvenComponent,
 
   ],
   imports: [
