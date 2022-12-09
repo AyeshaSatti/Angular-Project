bbbb8d381fb7dc519fbc23af75557f86092d122b
AyeshaSatti <ayeshasaghir@gmail.com>
2022-12-09 17:38:05 +0500

    Routing Learning

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -10,25 +10,34 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, Router, RouterModule } from '@angular/router'
-import { AppRoutingModule } from './app.routing.module'
+
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: 'users', component: UsersComponent,
+        children: [
+            { path: ':id/:name', component: UserComponent },
+        ]
+    },
+
+    {
+        path: 'servers', component: ServersComponent, children: [
+            { path: ':id', component: ServerComponent },
+            { path: ':id/edit', component: EditServerComponent }
+        ]
+    },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [ServersService],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(appRoutes),
+    ],
+    exports: [RouterModule],
+    providers: [ServersService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppRoutingModule { }
