25bc1516fe5542e603802f90e0eb65281aeb6a44
AyeshaSatti <ayeshasaghir@gmail.com>
2022-12-21 12:56:59 +0500

    Routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model'
-import { RecipeService } from '../../recipe.service';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -8,13 +7,8 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeItem!: Recipe;
+  @Input() index!: number;
 
-  constructor(private recipeService: RecipeService) { }
-
-  ngOnInit(): void {
-  }
-
-  showRecipe() {
-    this.recipeService.recipeSelected.emit(this.recipeItem);
+  ngOnInit(): void { 
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,12 +6,17 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'Test Recipe Description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyd0LJxe2agAj4xLXCr1V_rPIQkrEH8_3Akw&usqp=CAU'),
-        new Recipe('Another Recipe', 'Another Recipe Description', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyd0LJxe2agAj4xLXCr1V_rPIQkrEH8_3Akw&usqp=CAU')
+        new Recipe('Chicken Gravy ', 'Something like Fluidii', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyd0LJxe2agAj4xLXCr1V_rPIQkrEH8_3Akw&usqp=CAU'),
+        new Recipe('Mighty Burger', 'Buger Of Your Choice', 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80')
     ];
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+
     }
 
 }
